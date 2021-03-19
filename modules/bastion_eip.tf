21afd1a9d525588b1849b5afb0389946ee73682d
maximbek9999 <68786589+maximbek9999@users.noreply.github.com>
2021-03-19 18:25:14 -0500

    Delete modules directory

--- modules/bastion_eip.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_eip" "bastion_eip" {
-    vpc = true
-  }
-
-resource "aws_eip_association" "bastion_eip_assoc" {
-  instance_id   = aws_instance.bastion.id
-  allocation_id = aws_eip.bastion_eip.id
-}
--- modules/nat_gw.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_nat_gateway" "nat_gw" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.priv_sub_a.id
-  
-  tags = {
-    Name = "nat_gw"
-  }
-}
