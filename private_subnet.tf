8a89f96f4d87c67df8db949876206d000fee477f
maximbek9999 <68786589+maximbek9999@users.noreply.github.com>
2021-03-19 21:41:53 -0500

    This module create publick subnets.

---
+++ private_subnet.tf
@@ -0,0 +1,25 @@
+resource "aws_subnet" "priv_sub_a" {
+  vpc_id     = aws_vpc.wp_vpc.id
+  cidr_block = "10.0.11.0/24"
+
+  tags = {
+    Name = "priv_sub_a"
+  }
+}
+
+resource "aws_subnet" "priv_sub_b" {
+  vpc_id     = aws_vpc.wp_vpc.id
+  cidr_block = "10.0.12.0/24"
+
+  tags = {
+    Name = "priv_sub_b"
+  }
+}
+
+output "priv_sub_a_id" {
+  value = aws_subnet.priv_sub_a.id
+}
+
+output "priv_sub_b_id" {
+  value = aws_subnet.priv_sub_b.id
+}
