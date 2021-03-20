9564f1e2f011760e33a402bf77a4b451e0de2fe9
maximbek9999 <68786589+maximbek9999@users.noreply.github.com>
2021-03-19 19:17:14 -0500

    This module creates public route table.

---
+++ public_rt.tf
@@ -0,0 +1,22 @@
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.wp_vpc.id
+  
+route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id     = aws_internet_gateway.internet_gw.id
+  }
+
+  tags = {
+  Name = "public_rt"
+  }
+}
+resource "aws_route_table_association" "pub_sub_a" {
+  subnet_id      = aws_subnet.pub_sub_a.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "pub_sub_b" {
+  subnet_id      = aws_subnet.pub_sub_b.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
