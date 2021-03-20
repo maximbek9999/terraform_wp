aefdffa91fd68918ef84e02edf948a1a7f380410
maximbek9999 <68786589+maximbek9999@users.noreply.github.com>
2021-03-19 19:08:39 -0500

    This modules creates vpc with c pecific cidr_block, with enabled dns_support and vpc id output.

---
+++ vpc.tf
@@ -0,0 +1,12 @@
+resource "aws_vpc" "wp_vpc" {
+  cidr_block = "10.0.0.0/16"
+  instance_tenancy = "default"
+  enable_dns_support = true
+  
+  tags = {
+    Name = "wp_vpc"
+    }
+}
+output "vpc_id" {
+  value = aws_vpc.wp_vpc.id
+}
