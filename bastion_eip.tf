3185312a0779bc6e9d3890a113ba0aef976e745c
maximbek9999 <68786589+maximbek9999@users.noreply.github.com>
2021-03-21 13:57:43 -0500

    This module creates eip for bastion ec2.

---
+++ bastion_eip.tf
@@ -0,0 +1,13 @@
+resource "aws_eip" "bastion_eip" {
+  vpc = true
+
+}
+
+resource "aws_eip_association" "bastion_eip_assoc" {
+  instance_id   = aws_instance.bastion.id
+  allocation_id = aws_eip.bastion_eip.id
+}
+
+output "bastion_eip_id" {
+  value = aws_eip.bastion_eip.id
+}
